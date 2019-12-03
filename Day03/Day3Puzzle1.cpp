182ff566f88f2256044b06fc60c945e41ae88ce6
PowerSlaveAlfons <stefan.alfons@icloud.com>
2019-12-03 17:40:40 +0100

    Puzzle 1 solved.

--- Day03/Day3Puzzle1.cpp
+++ Day03/Day3Puzzle1.cpp
@@ -4,41 +4,46 @@
 #include <vector>
 #include <algorithm>
 
-void moveWire1(std::string input, std::vector<std::pair<int, int>> *VisitedSpots, signed int *x, signed int *y)
+void moveWire1(std::string input, std::vector<std::pair<long long, long long>> *VisitedSpots, signed int *x, signed int *y)
 {
 
     char Direction = input[0];
     input.erase(0, 1);
     int size = 0;
+    std::cout << "Input: " << input << std::endl;
 
     switch (Direction)
     {
     case 'U':
-        *y += std::stoi(input);
-        size = VisitedSpots->size();
-        VisitedSpots->push_back(std::make_pair(*x, *y));
-        std::cout << VisitedSpots->at(VisitedSpots->back()).first << " and " VisitedSpots->at(VisitedSpots.back).second << std::endl;
+        for (int i = 0; i < std::stoi(input); i++)
+        {
+            *y += 1;
+            VisitedSpots->push_back(std::make_pair(*x, *y));
+        }
         break;
 
     case 'L':
-        *x -= std::stoi(input);
-        size = VisitedSpots->size();
-        VisitedSpots->push_back(std::make_pair(*x, *y));
-        std::cout << VisitedSpots->at(VisitedSpots->back()).first << " and " VisitedSpots->at(VisitedSpots.back).second << std::endl;
+        for (int i = 0; i < std::stoi(input); i++)
+        {
+            *x -= 1;
+            VisitedSpots->push_back(std::make_pair(*x, *y));
+        }
         break;
 
     case 'D':
-        *y -= std::stoi(input);
-        size = VisitedSpots->size();
-        VisitedSpots->push_back(std::make_pair(*x, *y));
-        std::cout << VisitedSpots->at(VisitedSpots->back()).first << " and " VisitedSpots->at(VisitedSpots.back).second << std::endl;
+        for (int i = 0; i < std::stoi(input); i++)
+        {
+            *y -= 1;
+            VisitedSpots->push_back(std::make_pair(*x, *y));
+        }
         break;
 
     case 'R':
-        *x += std::stoi(input);
-        size = VisitedSpots->size();
-        VisitedSpots->push_back(std::make_pair(*x, *y));
-        std::cout << VisitedSpots->at(VisitedSpots->back()).first << " and " VisitedSpots->at(VisitedSpots.back).second << std::endl;
+        for (int i = 0; i < std::stoi(input); i++)
+        {
+            *x += 1;
+            VisitedSpots->push_back(std::make_pair(*x, *y));
+        }
         break;
 
     default:
@@ -47,64 +52,84 @@ void moveWire1(std::string input, std::vector<std::pair<int, int>> *VisitedSpots
     }
 }
 
-std::pair<int, int> moveWire2(std::string input, std::vector<std::pair<int, int>> *VisitedSpots, signed int *x, signed int *y)
+void moveWire2(std::string input, std::vector<std::pair<long long, long long>> *VisitedSpots, std::vector<std::pair<long long, long long>> *Intersections, signed int *x, signed int *y)
 {
-    std::vector<std::pair<int, int>> Intersections;
-    std::pair<int, int> PairToFind;
+    std::pair<long long, long long> PairToFind;
     char Direction = input[0];
     input.erase(0, 1);
+    std::cout << "Input: " << input << std::endl;
 
     switch (Direction)
     {
     case 'U':
-        PairToFind = std::make_pair(*x, *y);
-        *y += std::stoi(input);
-        for (auto Elem : *VisitedSpots)
+        for (int i = 0; i < std::stoi(input); i++)
         {
-            if (PairToFind == Elem)
+            *y += 1;
+            PairToFind = std::make_pair(*x, *y);
+            //std::cout << PairToFind.first << " and " << PairToFind.second << std::endl;
+            for (auto Elem : *VisitedSpots)
             {
-                std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
-                return PairToFind;
+                // std::cout << "Comparing " << Elem.first << " and " << Elem.second << " to " << PairToFind.first << " and " << PairToFind.second << std::endl;
+                if (PairToFind == Elem)
+                {
+                    std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
+                    Intersections->push_back(Elem);
+                }
             }
         }
 
         break;
 
     case 'L':
-        *x -= std::stoi(input);
-        PairToFind = std::make_pair(*x, *y);
-        for (auto Elem : *VisitedSpots)
+        for (int i = 0; i < std::stoi(input); i++)
         {
-            if (PairToFind == Elem)
+            *x -= 1;
+            PairToFind = std::make_pair(*x, *y);
+            //std::cout << PairToFind.first << " and " << PairToFind.second << std::endl;
+            for (auto Elem : *VisitedSpots)
             {
-                std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
-                return PairToFind;
+                // std::cout << "Comparing " << Elem.first << " and " << Elem.second << " to " << PairToFind.first << " and " << PairToFind.second << std::endl;
+                if (PairToFind == Elem)
+                {
+                    std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
+                    Intersections->push_back(Elem);
+                }
             }
         }
         break;
 
     case 'D':
-        PairToFind = std::make_pair(*x, *y);
-        *y -= std::stoi(input);
-        for (auto Elem : *VisitedSpots)
+        for (int i = 0; i < std::stoi(input); i++)
         {
-            if (PairToFind == Elem)
+            *y -= 1;
+            PairToFind = std::make_pair(*x, *y);
+            //std::cout << PairToFind.first << " and " << PairToFind.second << std::endl;
+            for (auto Elem : *VisitedSpots)
             {
-                std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
-                return PairToFind;
+                // std::cout << "Comparing " << Elem.first << " and " << Elem.second << " to " << PairToFind.first << " and " << PairToFind.second << std::endl;
+                if (PairToFind == Elem)
+                {
+                    std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
+                    Intersections->push_back(Elem);
+                }
             }
         }
         break;
 
     case 'R':
-        PairToFind = std::make_pair(*x, *y);
-        *x += std::stoi(input);
-        for (auto Elem : *VisitedSpots)
+        for (int i = 0; i < std::stoi(input); i++)
         {
-            if (PairToFind == Elem)
+            *x += 1;
+            PairToFind = std::make_pair(*x, *y);
+            //std::cout << PairToFind.first << " and " << PairToFind.second << std::endl;
+            for (auto Elem : *VisitedSpots)
             {
-                std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
-                return PairToFind;
+                // std::cout << "Comparing " << Elem.first << " and " << Elem.second << " to " << PairToFind.first << " and " << PairToFind.second << std::endl;
+                if (PairToFind == Elem)
+                {
+                    std::cout << "Found intersection at " << Elem.first << " " << Elem.second << std::endl;
+                    Intersections->push_back(Elem);
+                }
             }
         }
         break;
@@ -113,7 +138,6 @@ std::pair<int, int> moveWire2(std::string input, std::vector<std::pair<int, int>
         std::cout << "Illegal Move" << std::endl;
         break;
     }
-    return std::make_pair(0, 0);
 }
 
 int main()
@@ -125,20 +149,20 @@ int main()
     std::vector<std::string> tokens;
     std::vector<std::string> Wire1Instructions;
     std::vector<std::string> Wire2Instructions;
-    std::vector<std::pair<int, int>> VisitedSpots;
+    std::vector<std::pair<long long, long long>> VisitedSpots; //I cannot read ... I thought I had the wrong answer, and was confused about getting negative values.
+    std::vector<std::pair<long long, long long>> Intersections; //After converting to long and running it again I realized I was looking at the wrong thing.
+    std::vector<long long> ManhattenDistances;
     const std::string delim = ",";
     size_t pos = 0;
     signed int x = 0;
     signed int y = 0;
-    std::vector<std::pair<int, int>> Intersections;
+    int Counter = 0;
+    int Iterator = 0;
 
     if (myfile.is_open())
     {
 
-        for (std::string each; std::getline(myfile, each, '\n'); tokens.push_back(each))
-            ;
-
-        //std::cout << tokens[0] << std::endl;
+        for (std::string each; std::getline(myfile, each, '\n'); tokens.push_back(each));
 
         while ((pos = tokens[0].find(',')) != std::string::npos)
         {
@@ -152,6 +176,7 @@ int main()
             auxString = tokens[1].substr(0, pos);
             Wire2Instructions.push_back(auxString);
             tokens[1].erase(0, pos + delim.length());
+            Counter++;
         }
         Wire2Instructions.push_back(tokens[1]);
     }
@@ -167,15 +192,22 @@ int main()
 
     for (auto str : Wire2Instructions)
     {
-        Intersections.push_back(moveWire2(str, &VisitedSpots, &x, &y));
+        moveWire2(str, &VisitedSpots, &Intersections, &x, &y);
+        std::cout << Iterator << " of " << Counter << " Steps." << std::endl;
+        Iterator++;
     }
 
-    //std::cout << "Tokensize: " << tokens.size() << std::endl;
+    //sort( vec.begin(), vec.end() );
+    //vec.erase( unique( vec.begin(), vec.end() ), vec.end() );
+
+    std::cout << "Tokensize: " << tokens.size() << std::endl;
 
     for (auto pair : Intersections)
     {
         std::cout << pair.first << " and " << pair.second << std::endl;
+        ManhattenDistances.push_back(abs(pair.first) + abs(pair.second));
     }
-
+    std::sort(ManhattenDistances.begin(), ManhattenDistances.end());
+    std::cout << "Minimum Distance: " << ManhattenDistances[0] << std::endl;
     return 0;
 }
